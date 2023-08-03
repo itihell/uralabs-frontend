ab44f1f700bd1790ab981a845c3c8e393bdc89d5
Itihell Mejia <itihell.mejia@gmail.com>
2023-08-02 19:10:30 -0600

    Primer commit del frontend

---
+++ src/app/(setup)/roles/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState } from "react";
+
+export default function RolesPage() {
+  let [numero1, setNumero1] = useState(0);
+  let [numero2, setNumero2] = useState(0);
+  let [resultado, setResultado] = useState(0);
+  const sumarDosNumeros = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <form className="" onSubmit={sumarDosNumeros}>
+      <div className="h-auto">
+        <h1>Listado de roles</h1>
+        <div className="flex flex-row">
+          <label className="basis-3/12 text-right">Número 1</label>
+          <div className="basis-9/12 ml-1">
+            <input
+              type="number"
+              name="numero1"
+              id="numero1"
+              value={String(numero1)}
+              onChange={(e) => setNumero1(parseInt(e.target.value))}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row mt-2">
+          <label className="basis-3/12 text-right">Número 2</label>
+          <div className="basis-9/12 ml-1">
+            <input
+              type="number"
+              name="numero2"
+              id="numero2"
+              value={String(numero2)}
+              onChange={(e) => setNumero2(parseInt(e.target.value))}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row mt-2">
+          <button type="submit" className="bg-blue-500 text-white p-1">
+            Sumar
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+export default function AboutPage() {
+  return (
+    <div className="">
+      <div className="flex flex-row">
+        <div className=" bg-red-500 basis-1/6">01</div>
+        <div className="bg-gray-500 basis-5/6">03</div>
+      </div>
+
+      <div className="grid grid-cols-6">
+        <div className=" bg-red-500 ">01</div>
+        <div className="bg-gray-500  col-span-5">03</div>
+      </div>
+      <div className="flex">
+        <div className=" bg-red-500 w-1/6">01</div>
+        <div className="bg-gray-500 w-5/6">03</div>
+      </div>
+    </div>
+  );
+}
