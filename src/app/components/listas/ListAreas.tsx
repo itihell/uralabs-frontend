35d1188c79b65ef5668b3937ca9a9571c0e2d47b
TaniaCruz09 <cruztania0909@gmail.com>
2023-10-22 00:17:24 -0600

    Agregando la lista de Areas

--- src/app/components/listas/ListAreas.tsx
+++ src/app/components/listas/ListAreas.tsx
@@ -12,8 +12,6 @@ export default async function ListAreas() {
     <div className="my-8">
     <h1>Lista de Areas</h1>
     <select
-    id="area"
-    name="area"
     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     >
       {dataRoles.map((area: any) => {
