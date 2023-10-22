67a37e2dd3c6ab010ba0ccee575b9af91a35b573
jefferson <rodriguezjef9@gmail.com>
2023-10-22 00:45:36 -0600

    mojorando el diseño

--- src/app/components/listas/ListCortePracticante copy.tsx
+++ src/app/components/listas/ListCortePracticante copy.tsx
@@ -12,7 +12,9 @@ export default async function ListCortePracticante() {
   return (
     <div className="my-8">
       <h1>Lista de Cote de Practica</h1>
-      <select>
+      <select
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
       {dataPracticante.map((practicante: any) => {
         return <option key={`practicante-${practicante.id}`}>{practicante.practicante}</option>;
       })}
