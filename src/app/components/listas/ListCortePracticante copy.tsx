5cafa595cc7375dea1ff493dac8ed440244ef193
jefferson <rodriguezjef9@gmail.com>
2023-10-22 00:53:59 -0600

    corigiendo ortografia

--- src/app/components/listas/ListCortePracticante copy.tsx
+++ src/app/components/listas/ListCortePracticante copy.tsx
@@ -11,7 +11,7 @@ export default async function ListCortePracticante() {
   const dataPracticante = await getPracticante();
   return (
     <div className="my-8">
-      <h1>Lista de Cote de Practica</h1>
+      <h1>Lista de Corte de Practica</h1>
       <select
       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
