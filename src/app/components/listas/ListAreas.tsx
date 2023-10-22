1b7b6558a2ff19896fd948b08681d82bc31d4bda
TaniaCruz09 <cruztania0909@gmail.com>
2023-10-21 23:49:58 -0600

    Agregando la lista de Areas

--- src/app/components/listas/ListAreas.tsx
+++ src/app/components/listas/ListAreas.tsx
@@ -9,14 +9,17 @@ export default async function ListAreas() {
 
   const dataRoles = await getAllAreas();
   return (
+    <div className="my-8">
+    <h1>Lista de Areas</h1>
     <select
     id="area"
     name="area"
-    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 my-8"
+    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     >
       {dataRoles.map((area: any) => {
         return <option key={`area-${area.id}`}>{area.nombre}</option>;
       })}
     </select>
+    </div>
   );
 }
