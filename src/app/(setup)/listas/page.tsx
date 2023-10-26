375ac00e7580ae42eb70daa1868c7a19d58c8897
jasson <jeicobfer@gmail.com>
2023-10-25 18:54:19 -0600

    agragado lista de usuarios

--- src/app/(setup)/listas/page.tsx
+++ src/app/(setup)/listas/page.tsx
@@ -4,6 +4,7 @@ import ListAreas from "@/app/components/listas/ListAreas";
 import ListCortePracticante from "@/app/components/listas/ListCortePracticante";
 import ListPracticante from "@/app/components/listas/ListPracticante";
 import ListRoles from "@/app/components/listas/ListRoles";
+import ListUseres from "@/app/components/listas/ListUsers";
 
 export default function ListasPages() {
   return (
@@ -14,6 +15,7 @@ export default function ListasPages() {
       <ListAreas />
       <ListCortePracticante />
       <ListPracticante />
+      <ListUseres/>
     </div>
   );
 }
