63dbe92a7a8a4837bd60fbac5d5de10af6729723
Itihell Mejia <itihell.mejia@gmail.com>
2023-10-25 19:21:18 -0600

    fix errores de listas

--- src/app/(setup)/listas/page.tsx
+++ src/app/(setup)/listas/page.tsx
@@ -1,26 +1,14 @@
 
-import ListModalidades from "@/app/components/listas/ListModalidades";
-import ListAreas from "@/app/components/listas/ListAreas";
-import ListCortePracticante from "@/app/components/listas/ListCortePracticante";
-import ListPracticante from "@/app/components/listas/ListPracticante";
+
 import ListRoles from "@/app/components/listas/ListRoles";
-import ListUseres from "@/app/components/listas/ListUsers";
-import ListUsoLab from "@/app/components/listas/ListUsoLab";
-import ListLaboratory from "@/app/components/listas/ListLaboratory";
+
 
 export default function ListasPages() {
   return (
     <div className="min-h-screen flex-col items-center justify-between">
       <h1>Listas Pages</h1>
       <ListRoles />
-      <ListModalidades />
-      <ListAreas />
-      <ListCortePracticante />
-      <ListPracticante />
-      <ListUseres/>
-      <ListUsoLab/>
-      <ListLaboratory />
-
+     
     </div>
   );
 }
