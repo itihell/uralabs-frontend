570e04483052c2b23428d95135e0bd46fdc6c129
Bryttem <bryttem.brytton.mejia@gmail.com>
2023-11-01 20:15:25 -0600

    cambios

---
+++ src/app/components/select/select-item.tsx
@@ -0,0 +1,5 @@
+export default function SelectItem(){
+    return (
+        <div></div>
+    )
+}
---
+++ src/app/components/select/select.tsx
@@ -0,0 +1,8 @@
+'use client'
+export default function SelectSearch(){
+    return (
+        <div>
+            <h1>Listas</h1>
+        </div>
+    )
+}
