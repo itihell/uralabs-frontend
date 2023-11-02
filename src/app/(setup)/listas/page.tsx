785ae9c29c93a846b409b98ac6745942650d8869
Avimilex <95236561+Requenez-Diaz@users.noreply.github.com>
2023-11-01 23:30:07 -0600

    agregando las listas de modalidades

--- src/app/(setup)/listas/page.tsx
+++ src/app/(setup)/listas/page.tsx
@@ -1,10 +1,14 @@
+import ListModalidades from "@/app/components/listas/ListModalidades";
 import ListRoles from "@/app/components/listas/ListRoles";
 
 export default function ListasPages() {
   return (
-    <div className="min-h-screen flex-col items-center justify-between">
+    <div className='min-h-screen flex-col items-center justify-between'>
       <h1>Listas Pages</h1>
       <ListRoles />
+      <hr />
+      <h1>Modalidades</h1>
+      <ListModalidades />
     </div>
   );
 }
