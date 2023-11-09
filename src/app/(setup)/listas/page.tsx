db1e576e27b82716a8093f728708a362c7d00904
Jasson Fernandez <jeicobfer@gmail.com>
2023-11-08 19:24:06 -0600

    realizando los nuevos cambios

--- src/app/(setup)/listas/page.tsx
+++ src/app/(setup)/listas/page.tsx
@@ -12,6 +12,9 @@ export default function ListasPages() {
   const changeRole = (e: any) => {
     console.log("desde la pagina ", e.id);
   };
+  const changeUser = (e: any) => {
+    console.log("desde la pagina ", e.id);
+  }
   return (
     <div className="min-h-screen flex-col items-center justify-between">
       <h1>Listas Pages</h1>
@@ -33,7 +36,7 @@ export default function ListasPages() {
       <ListPracticante />
 
       <h1>Lista de Usuarios</h1>
-      <ListUsers />
+      <ListUsers selected={changeUser} />
       <h1>Lista de Carreras</h1>
       <ListCarrera />
     </div>
