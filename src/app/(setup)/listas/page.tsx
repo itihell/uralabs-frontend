d7d656a21f53e16541b38af14b6d8a2e4c09a978
Jasson Fernandez <jeicobfer@gmail.com>
2023-11-08 20:21:42 -0600

    json de list

--- src/app/(setup)/listas/page.tsx
+++ src/app/(setup)/listas/page.tsx
@@ -7,33 +7,44 @@ import ListRoles from "@/app/components/listas/ListRoles";
 import ListUsers from "@/app/components/listas/ListUsers";
 import ListCarrera from "@/app/components/listas/ListaCarrera";
 import ListUsoLab from "@/app/components/listas/ListUsoLab";
+import { useState } from "react";
 
 export default function ListasPages() {
+  const [filds, setFilds] = useState<any>({});
   const changeRole = (e: any) => {
+    setFilds({ ...filds, role_id: e.id })
     console.log("desde la pagina ", e.id);
   };
   const changeUsolab = (e: any) => {
+    setFilds({ ...filds, usolab_id: e.id })
     console.log("desde la pagina ", e.id);
   };
 
   const changeArea = (e: any) => {
+    setFilds({ ...filds, area_id: e.id })
     console.log("desde la pagina ", e.id);
   };
 
   const changeUser = (e: any) => {
+    setFilds({ ...filds, user_id: e.id })
     console.log("desde la pagina ", e.id);
   }
 
   const changePracticante = (e: any) => {
+    setFilds({ ...filds, practicante_id: e.id })
     console.log("desde la pagina ", e.id);
   };
   
   const changeCarrea = (e: any) => {
+    setFilds({ ...filds, carrera_id: e.id })
     console.log("desde la pagina ", e.id);
   };
 
   return (
     <div className="min-h-screen flex-col items-center justify-between">
+      <pre>
+        {JSON.stringify(filds, null, 2)}
+      </pre>
       <h1>Listas Pages</h1>
       <ListRoles selected={changeRole} />
       <hr />
@@ -51,7 +62,7 @@ export default function ListasPages() {
 
       <h1>Lista de Practicante</h1>
       <ListPracticante selected={changePracticante} />
-
+      
       <h1>Lista de Usuarios</h1>
       <ListUsers selected={changeUser} />
 
