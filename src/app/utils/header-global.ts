17b88f98108882a07dd225b955fedb6257d24ddd
Itihell Mejia <itihell.mejia@gmail.com>
2023-11-16 11:20:45 -0600

    ACtualizando la cabecera

--- src/app/utils/header-global.ts
+++ src/app/utils/header-global.ts
@@ -1,8 +1,8 @@
-"use server";
 const getHeadersGlobal = () => {
+  //const apiKey = 'probando';
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
   const headers = new Map();
-  headers.set("Content-Type", "application/json");
+  headers.set("Content-Type", "application/json; charset=utf-8");
   headers.set("api-key", apiKey);
 
   return headers;
