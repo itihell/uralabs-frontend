20c717bc7700a7df23e5e2f000ee7b3ceb9b7acf
Itihell Mejia <itihell.mejia@gmail.com>
2023-11-16 11:20:26 -0600

    Eliminando archivo no usados

--- src/app/(setup)/rolles/add/page.tsx
+++
@@ -1,8 +0,0 @@
-import FormRoles from "../../../components/forms/roles/form-roles";
-export default function AddRolePage() {
-  return (
-    <div className='min-h-screen'>
-      <FormRoles />
-    </div>
-  );
-}
--- src/app/(setup)/rolles/edit/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-// import FormEditRole from "@/app/components/forms/roles/form-edit-role";
-
-export default function EditRolePage({ params }: { params: { id: number } }) {
-  return (
-    <div className='min-h-screen'>
-      {/* <FormEditRole roleId={params.id} /> */}
-    </div>
-  );
-}
