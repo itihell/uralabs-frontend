d2ddd925efa60f22491747a86912c9a4030ddff5
Arlen Assael Marenco Jaime <91499118+NightCW34@users.noreply.github.com>
2023-11-21 14:58:40 -0600

    Add Delete and Update to Turnos Module

--- src/app/turnos/TurnosPage.tsx
+++ src/app/turnos/TurnosPage.tsx
@@ -9,8 +9,9 @@ interface Turno {
 const Turnos = (): JSX.Element => {
   const [turnos, setTurnos] = useState<Turno[]>([]);
   const [nuevoTurno, setNuevoTurno] = useState<string>("");
+  const [editTurno, setEditTurno] = useState<Turno | null>(null);
+  const [editTurnoName, setEditTurnoName] = useState<string>(""); // Nuevo estado para el input de edición
 
-  // Obtener la lista de turnos al cargar la página
   useEffect(() => {
     const fetchTurnos = async () => {
       try {
@@ -25,7 +26,6 @@ const Turnos = (): JSX.Element => {
     fetchTurnos();
   }, []);
 
-  // Función para agregar un nuevo turno
   const handleAgregarTurno = async () => {
     try {
       const response = await fetch("http://localhost:5003/turnos", {
@@ -45,6 +45,56 @@ const Turnos = (): JSX.Element => {
     }
   };
 
+  const handleEditarTurno = (turno: Turno) => {
+    setEditTurno(turno);
+    setEditTurnoName(turno.name); // Inicializar el estado del input de edición con el nombre actual del turno
+    setNuevoTurno(""); // Limpiar el input de agregar turno
+  };
+
+  const handleActualizarTurno = async () => {
+    try {
+      if (editTurno) {
+        const response = await fetch(
+          `http://localhost:5003/turnos/${editTurno.id}`,
+          {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name: editTurnoName }), // Usar editTurnoName en lugar de nuevoTurno
+          }
+        );
+
+        const updatedTurno: Turno = await response.json();
+
+        setTurnos((prevTurnos) =>
+          prevTurnos.map((t) => (t.id === updatedTurno.id ? updatedTurno : t))
+        );
+
+        setEditTurno(null);
+        setNuevoTurno("");
+      }
+    } catch (error) {
+      console.error("Error updating turno:", error);
+    }
+  };
+
+  const handleEliminarTurno = async (id: number) => {
+    try {
+      const response = await fetch(`http://localhost:5003/turnos/${id}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        setTurnos((prevTurnos) => prevTurnos.filter((t) => t.id !== id));
+      } else {
+        console.error("Error deleting turno");
+      }
+    } catch (error) {
+      console.error("Error deleting turno:", error);
+    }
+  };
+
   return (
     <div className="container mx-auto my-8">
       <h1 className="text-3xl font-bold mb-4">Turnos</h1>
@@ -53,13 +103,60 @@ const Turnos = (): JSX.Element => {
           <tr>
             <th className="py-2 px-4 border-b">ID</th>
             <th className="py-2 px-4 border-b">Nombre</th>
+            <th className="py-2 px-4 border-b">Acciones</th>
           </tr>
         </thead>
         <tbody>
           {turnos.map((turno) => (
             <tr key={turno.id}>
               <td className="py-2 px-4 border-b">{turno.id}</td>
-              <td className="py-2 px-4 border-b">{turno.name}</td>
+              <td className="py-2 px-4 border-b">
+                {editTurno?.id === turno.id ? (
+                  <input
+                    type="text"
+                    value={editTurnoName}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                      setEditTurnoName(e.target.value)
+                    }
+                    className="p-2 border border-gray-300 bg-gray-100 text-gray-800 mr-2"
+                  />
+                ) : (
+                  turno.name
+                )}
+              </td>
+              <td className="py-2 px-4 border-b">
+                {editTurno?.id === turno.id ? (
+                  <>
+                    <button
+                      onClick={handleActualizarTurno}
+                      className="bg-green-500 text-white py-1 px-2 rounded mr-2"
+                    >
+                      Guardar
+                    </button>
+                    <button
+                      onClick={() => setEditTurno(null)}
+                      className="bg-gray-500 text-white py-1 px-2 rounded"
+                    >
+                      Cancelar
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button
+                      onClick={() => handleEditarTurno(turno)}
+                      className="bg-blue-500 text-white py-1 px-2 rounded mr-2"
+                    >
+                      Editar
+                    </button>
+                    <button
+                      onClick={() => handleEliminarTurno(turno.id)}
+                      className="bg-red-500 text-white py-1 px-2 rounded"
+                    >
+                      Borrar
+                    </button>
+                  </>
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
@@ -71,7 +168,7 @@ const Turnos = (): JSX.Element => {
           onChange={(e: ChangeEvent<HTMLInputElement>) =>
             setNuevoTurno(e.target.value)
           }
-          className="p-2 border border-gray-300 mr-2"
+          className="p-2 border border-gray-300 bg-gray-100 text-gray-800 mr-2"
         />
         <button
           onClick={handleAgregarTurno}
