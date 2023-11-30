87e9eb05a46931176734c62a77ffe44aa423fbea
Avimilex <95236561+Requenez-Diaz@users.noreply.github.com>
2023-11-30 16:34:16 -0600

    adding interface and hooks to modalidades

--- src/app/(setup)/rolles/loading.tsx
+++ src/app/(setup)/rolles/loading.tsx
@@ -4,42 +4,42 @@ import { Button, Skeleton } from "@nextui-org/react";
 export default function LoadingPage() {
   return (
     <div>
-      <div className="min-h-screen">
-        <h1 className="mb-3">Listado de roles</h1>
+      <div className='min-h-screen'>
+        <h1 className='mb-3'>Listado de roles</h1>
 
-        <div className="mb-3 flex content-end justify-items-end">
-          <Skeleton className="rounded-lg">
-            <Button variant="light" size="sm" color="primary">
+        <div className='mb-3 flex content-end justify-items-end'>
+          <Skeleton className='rounded-lg'>
+            <Button variant='light' size='sm' color='primary'>
               Agregar Rol
             </Button>
           </Skeleton>
         </div>
 
         <div>
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <Skeleton className="w-full">
-              <div className="w-full h-10">Probando</div>
+          <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+            <Skeleton className='w-full'>
+              <div className='w-full h-10'>Probando</div>
             </Skeleton>
-            <Skeleton className="w-full border-t-0 mt-0">
-              <div className="w-full h-7">Probando</div>
+            <Skeleton className='w-full border-t-0 mt-0'>
+              <div className='w-full h-7'>Probando</div>
             </Skeleton>
-            <Skeleton className="w-full border-t-0 mt-0">
-              <div className="w-full h-7">Probando</div>
+            <Skeleton className='w-full border-t-0 mt-0'>
+              <div className='w-full h-7'>Probando</div>
             </Skeleton>
-            <Skeleton className="w-full border-t-0">
-              <div className="w-full h-7">Probando</div>
+            <Skeleton className='w-full border-t-0'>
+              <div className='w-full h-7'>Probando</div>
             </Skeleton>
-            <Skeleton className="w-full border-t-0">
-              <div className="w-full h-7">Probando</div>
+            <Skeleton className='w-full border-t-0'>
+              <div className='w-full h-7'>Probando</div>
             </Skeleton>
-            <Skeleton className="w-full border-t-0">
-              <div className="w-full h-7">Probando</div>
+            <Skeleton className='w-full border-t-0'>
+              <div className='w-full h-7'>Probando</div>
             </Skeleton>
-            <Skeleton className="w-full border-t-0">
-              <div className="w-full h-7">Probando</div>
+            <Skeleton className='w-full border-t-0'>
+              <div className='w-full h-7'>Probando</div>
             </Skeleton>
-            <Skeleton className="w-full border-t-0">
-              <div className="w-full h-7">Probando</div>
+            <Skeleton className='w-full border-t-0'>
+              <div className='w-full h-7'>Probando</div>
             </Skeleton>
           </div>
         </div>
---
+++ src/app/interfaces/modalidades-interface.ts
@@ -0,0 +1,6 @@
+export interface Modalidades {
+  id?: number;
+  modalidade: string;
+  is_active: boolean;
+  isActive?: string;
+}
