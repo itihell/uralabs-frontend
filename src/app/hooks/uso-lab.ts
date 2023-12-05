ddb091480a1011ecb2fc68bd3b76d897339706c7
Mitch-Luna <lunitamitch02@gmail.com>
2023-12-04 21:01:50 -0600

    verificnado algunas validaciones

--- src/app/hooks/uso-lab.ts
+++ src/app/hooks/uso-lab.ts
@@ -3,26 +3,31 @@ import feching from "../utils/cliente-http";
 
 export function useLaboratorio() {
   const onStore = async (payload: UsoLab) => {
-    const { male, female, className,carrera,docente,modality,laboratorio,...restPayload } = payload;
-    const total = Number(male)+Number(female);
+    const { male, female, className, carrera, docente, modality, laboratorio, ...restPayload } = payload;
+  
+    // Asegurémonos de que las propiedades problemáticas sean objetos completos
+    const formattedPayload = {
+      ...restPayload,
+      total: Number(male) + Number(female),
+      male: Number(male),
+      female: Number(female),
+      className: { id: Number(className) },  // Ajusta el formato según lo que espere el servidor
+      carrera: { id: Number(carrera) },      // Ajusta el formato según lo que espere el servidor
+      docente: { id: Number(docente) },      // Ajusta el formato según lo que espere el servidor
+      modality: { id: Number(modality) },    // Ajusta el formato según lo que espere el servidor
+      laboratorio: { id: Number(laboratorio) },  // Ajusta el formato según lo que espere el servidor
+    };
+  
+    console.log(formattedPayload);
+  
     const url = `/uselab`;
   
-    const updatePayload = {
-      ...restPayload,
-      total,
-      male:Number(male),  
-      female:Number(female),
-      carrera: Number(carrera),
-      docente: Number(docente),
-      modality: Number(modality),
-      laboratorio: Number(laboratorio),
-      className: Number(className),
-    };
-    console.log(updatePayload);
-    const rest = await feching(url, "no-store", "POST", updatePayload);
+    const rest = await feching(url, "no-store", "POST", formattedPayload);
+  
     console.log(rest);
     return rest;
   };
+  
 
   const onShowAll = async (id: number) => {
     const url = `/uselab?z=1${id}`;
--- src/app/interfaces/usoLab-interfaces.ts
+++ src/app/interfaces/usoLab-interfaces.ts
@@ -1,29 +1,18 @@
-
 export interface UsoLab {
   id?: number;
-  className?: {
-    id: number;
-  };
-  carrera?: {
-    id: number;
-  };
-  docente?: {
-    id: number;
-  };
-  date?: string;
-  modality?: {
-    id: number;
-  };
+
+  className?: number;
+  carrera?: number;
+  docente?: number;
+  modality?: number;
+  laboratorio?: number;
   shift?: string;
   year?: number;
   semester?: string;
   female?: number;
   male?: number;
   hours?: string;
+ 
   is_active?: boolean;
   deleted_at?: string;
-  laboratorio?: {
-    id: number;
-  };
-
 }
