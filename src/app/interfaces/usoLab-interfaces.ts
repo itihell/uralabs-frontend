12fbd82ccad33cedc54a6207633a03ab1cc78590
Mitch-Luna <lunitamitch02@gmail.com>
2023-12-13 14:30:18 -0600

    changue en inputs

--- src/app/interfaces/usoLab-interfaces.ts
+++ src/app/interfaces/usoLab-interfaces.ts
@@ -1,13 +1,36 @@
 export interface UsoLab {
   id?: number;
 
-  className?: number;
+  className?: {
+    id: number;
+    nombre: string;
+  };
   date?: string;
-  carrera?: number;
-  docente?: number;
-  modality?: number;
-  laboratorio?: number;
-  shift?: number;
+  carrera?: {
+    id: number;
+    nombre: string;
+    area?: {
+      nombre: string;
+      id: number;
+    };
+  };
+  docente?: {
+    id: number;
+    nombre: string;
+    apellido: string;
+  };
+  modality?: {
+    id: number;
+    modalidad: string;
+  };
+  laboratorio?: {
+    id: number;
+    labName: string;
+  };
+  shift?: {
+    id: number;
+
+  };
   year?: number;
   semester?: string;
   female?: number;
