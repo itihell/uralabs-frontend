079ade6dba6c010d29eb7a16f10729f126ceeba5
Mitch-Luna <lunitamitch02@gmail.com>
2023-12-13 17:21:05 -0600

    changue

--- src/app/components/forms/usoLaboratorio/fields-UsoLabModal.tsx
+++ src/app/components/forms/usoLaboratorio/fields-UsoLabModal.tsx
@@ -1,7 +1,7 @@
 import { setterData } from "@/app/interfaces/setter-interfaces";
 import { UsoLab } from "@/app/interfaces/usoLab-interfaces";
 import { useEffect, useState } from "react";
-import { getAllAsignaturas} from "../../../(setup)/laboratory-use/../../(setup)/laboratory-use/actions/post/save-labUse";
+import {  getAllDocentes} from "../../../(setup)/laboratory-use/../../(setup)/laboratory-use/actions/post/save-labUse";
 
 interface FieldsUsoLabProps {
   usoLaboratorio?: UsoLab;
@@ -26,14 +26,14 @@ export default function FieldsUsoLabModal(
   }, [usoLaboratorio]);
 
 
-  const [asignaturas, setAsignaturas] = useState([]);
+  const [docentes, setDocentes] = useState([]);
 
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const asignaturaData = await getAllAsignaturas();
-        setAsignaturas(asignaturaData);
+        const docentesData = await getAllDocentes();
+        setDocentes(docentesData);
 
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -50,26 +50,27 @@ export default function FieldsUsoLabModal(
               <label htmlFor="className" className="block mb-4 text-sm font-medium text-gray-900 dark:text-white">Nombre de la Clase</label>
             </div>
             <select
-              id="className"
-              name="className"
-              onChange={(e) => {
-                const data: setterData = {
-                  clave: e.target.name,
-                  valor: e.target.value,
-                };
-                handleChangeUsoLab(data);
-              }}
-              className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 bg-gray-900"
-            >
-              <option value="className" >
-                Seleccionar Asignatura
-              </option>
-              {asignaturas.map((asignatura: any) => (
-                <option  key={asignatura.id} value={asignatura.id}  >
-                  {asignatura.nombre}
-                </option>
-              ))}
-            </select>
+          name="docente"
+          id="docente"
+          value={fields?.docente?.id }
+          onChange={(e) => {
+            const data: setterData = {
+              clave: e.target.name,
+              valor: e.target.value,
+            };
+            handleChangeUsoLab(data);
+          }}
+          className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 bg-gray-900"
+        >
+          <option value="docente" >
+            Seleccionar docente
+          </option>
+          {docentes.map((docente: any) => (
+            <option key={docente.id} value={docente.id}  >
+              {docente.nombre} {docente.apellido}
+            </option>
+          ))}
+        </select>
           </div>
     </div>
   );
